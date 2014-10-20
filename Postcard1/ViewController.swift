61582d78e321ce7d82ca84a63ef914530eddff3e
Yerzhan Assanov <ekazhman@gmail.com>
2014-10-20 23:01:42 +0600

    Added value of nameField to be displayed as label

--- Postcard1/ViewController.swift
+++ Postcard1/ViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var enterNameTextField: UITextField!
     @IBOutlet weak var enterMessageTextField: UITextField!
     
@@ -29,11 +30,19 @@ class ViewController: UIViewController {
 
     @IBAction func sendMailButtonPressed(sender: UIButton) {
         messageLabel.hidden = false
+        nameLabel.hidden  = false
+        
         messageLabel.text = enterMessageTextField.text
         messageLabel.textColor = UIColor.redColor()
         
         enterMessageTextField.text = ""
         enterMessageTextField.resignFirstResponder()
+        
+        nameLabel.text = "by " + enterNameTextField.text
+        nameLabel.textColor = UIColor.purpleColor()
+        
+        enterNameTextField.text = ""
+        enterNameTextField.resignFirstResponder()
         
         mailButton.setTitle("Mail Sent", forState: UIControlState.Normal)
     }
